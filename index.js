0f7ca0581d590965c633c83868fe3ff5e2df0de6
Christopher Randazzo <chris_rando2001@yahoo.com>
2018-10-11 23:33:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var uppercase = "HELLO!"
+  var uppercase = "HELLO"
   uppercase.toUpperCase() === uppercase
-  var lowercase = 'hello!'
+  var lowercase = 'hello'
   lowercase.toLowerCase() === lowercase
   var mixedCase = 'I love you, Grandma'
   mixedCase.toLowerCase() === mixedCase
